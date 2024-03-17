3004505d94e82652961e87efe1132215799cfc77
Surya <shubhamsurya54@gmail.com>
2024-03-18 01:21:23 +0530

    dockerized the signup service and added docker compose file

--- src/backend/signup/src/index.js
+++ src/backend/signup/src/index.js
@@ -6,8 +6,12 @@ const app = express();
 
 const { PORT } = require('./config/serverConfig')
 
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
 const prepareAndStartServer = () => {
-    app.listen(PORT, () => {
+    app.listen(PORT,() => {
         logger.info('This is an informational message');
         logger.warn('This is a warning message');
         logger.error('This is an error message');
