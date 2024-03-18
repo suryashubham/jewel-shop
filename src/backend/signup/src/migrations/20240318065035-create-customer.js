d29bf2b3ce5fc81970be8daa2222c403c83488a5
Surya <shubhamsurya54@gmail.com>
2024-03-18 14:19:48 +0530

    added customer model using sequelize orm.

---
+++ src/backend/signup/src/migrations/20240318065035-create-customer.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Customers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      dob: {
+        type: Sequelize.DATE
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      mobile: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      pin: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Customers');
+  }
+};
---
+++ src/backend/signup/src/models/customer.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Customer extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Customer.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dob: {
+      type: DataTypes.DATE
+    },
+    address: {
+      type: DataTypes.STRING
+    },
+    email: { 
+      type: DataTypes.STRING
+    },
+    mobile: { 
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    pin: { 
+      type: DataTypes.STRING
+    }
+  }, {
+    sequelize,
+    modelName: 'Customer',
+  });
+  return Customer;
+};
